b1e7160046d9de14fec09fd5456c7f1c7950c996
xiyang <sdlgxxy@gmail.com>
2013-03-10 22:36:48 +0800

    modify hook

--- diy/manage.py
+++ diy/manage.py
@@ -6,6 +6,13 @@
 ########################################################################
 # -*- coding: UTF-8 -*-
 #!/usr/bin/env python
+import os
+here = os.path.dirname(os.path.abspath(__file__))
+os.environ['PYTHON_EGG_CACHE'] = os.path.join(here, '..', 'misc/virtenv/lib/python2.7/site-packages')
+os.environ['PYTHONPATH'] = os.path.join(here, '..', 'misc/virtenv/lib/python2.7/site-packages')
+virtualenv = os.path.join(here, '..', 'misc/virtenv/bin/activate_this.py')
+execfile(virtualenv, dict(__file__=virtualenv))
+
 from flask.ext.script import Manager
 
 from yescloud import app
